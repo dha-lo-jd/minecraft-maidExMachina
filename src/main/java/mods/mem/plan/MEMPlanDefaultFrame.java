ce710504d3b62663c9a5af2aa92246e6e1cd5a83
dha_lo_jd <dha_1024_blame@hotmail.com>
2016-12-29 18:45:45 +0900

    お前さん、その様子だとこれで12回目のコミットのようだな？

---
+++ src/main/java/mods/mem/plan/MEMPlanDefaultFrame.java
@@ -0,0 +1,19 @@
+package mods.mem.plan;
+
+public class MEMPlanDefaultFrame {
+
+    int color;
+    String texture;
+
+    public MEMPlanDefaultFrame(int color, String texture) {
+        this.color = color;
+        this.texture = texture;
+    }
+
+    @Override
+    public void setInfo(EntityMEMFrame frame) {
+        frame.setColors(new int[]{color, color, color, color, color, color,});
+        frame.setTextures(new String[]{texture, texture, texture, texture,
+                texture, texture,});
+    }
+}
