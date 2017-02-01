43221f5af3305255bea5a604beb6b3ddb940f9a0
dha_lo_jd <dha_1024_blame@hotmail.com>
2017-02-01 22:43:27 +0900

    ＊コミットされたソースの匂いがする

---
+++ src/main/java/mods/mem/MEMSupport.java
@@ -0,0 +1,24 @@
+package mods.mem;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.util.ChunkCoordinates;
+import net.minecraft.world.World;
+import scala.Double;
+
+/**
+ * Created by dhaAsusAdmin on 2017/02/01.
+ */
+public class MEMSupport {
+    public static Double getAreaHeightFromGround(Set<ChunkCoordinates> area,
+                                                 World world, Set<Material> ignoreMaterials) {
+        for (int i = 0; i < 256; i++) {
+            for (ChunkCoordinates pos : area) {
+                if (!CoordsSupport.isNonMaterial(world, pos.posX, pos.posY - i,
+                        pos.posZ, ignoreMaterials)) {
+                    return (double) i;
+                }
+            }
+        }
+        return null;
+    }
+}
